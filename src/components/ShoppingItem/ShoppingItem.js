11b968690bf249d527bb70f832694ccdf587fcfa
RomanasV <romanasvenckus@gmail.com>
2022-11-23 13:18:42 +0200

    shopping list done event task

--- src/components/ShoppingItem/ShoppingItem.js
+++ src/components/ShoppingItem/ShoppingItem.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
 
 const ShoppingItem = ({title, done}) => {
-  // const [num, setNum] = useState(0);
-  // const [elementTitle, setElementTitle] = useState(title);
-
-  // function clickHandler() {
-  //   setNum(num + 1);
-  //   setElementTitle(title + ' labas');
-  // }
-
   const [isDone, setIsDone] = useState(done);
 
   function doneHandler() {
-    setIsDone(true);
+    setIsDone(!isDone);
   }
 
   return (
     <div className='shopping-item'>
-      {!isDone && <button onClick={doneHandler}>Done</button>}
       <span>{isDone && '✓ '}{title}</span>
+      <button onClick={doneHandler}>{isDone ? 'Remove' : 'Add'}</button>
     </div>
   )
 }
