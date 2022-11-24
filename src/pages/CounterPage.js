26d629d8ec95d72d0f44e17241540ee4e9119592
RomanasV <romanasvenckus@gmail.com>
2022-11-24 12:02:13 +0200

    reset button added

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react'
 
 const CounterPage = () => {
-  const [count, setCount] = useState(5);
+  const initialCount = 5;
+  const [count, setCount] = useState(initialCount);
   const countClass = count > 4 ? 'color-green' : 'color-red';
 
-  const counterHandler = (num) => setCount(prevState => prevState + num);
+  const counterHandler = num => setCount(prevState => prevState + num);
+  const disableButton = condition => condition ? true : null;
+  const counterInputHandler = event => setCount(event.target.value);
 
   return (
     <div style={{padding: '10px 30px 100px' }}>
       <div className={countClass}>{count}</div>
-      <button onClick={() => counterHandler(-2)} disabled={count < 3 ? true : null}>-2</button>
-      <button onClick={() => counterHandler(-1)} disabled={count < 2 ? true : null}>-1</button>
-      <button onClick={() => counterHandler(1)} disabled={count > 9 ? true : null}>+1</button>
-      <button onClick={() => counterHandler(2)} disabled={count > 8 ? true : null}>+2</button>
+      <input type='number' value={count} onChange={counterInputHandler} />
+      <div>
+        <button onClick={() => counterHandler(-2)} disabled={disableButton(count < 3)}>-2</button>
+        <button onClick={() => counterHandler(-1)} disabled={disableButton(count < 2)}>-1</button>
+        <button onClick={() => counterHandler(1)} disabled={disableButton(count > 9)}>+1</button>
+        <button onClick={() => counterHandler(2)} disabled={disableButton(count > 8)}>+2</button>
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+      </div>
     </div>
   )
 }
