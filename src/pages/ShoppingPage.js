b28dbe3fde8ee6698f5a74fc1e311580936e6bc0
RomanasV <romanasvenckus@gmail.com>
2022-11-25 10:01:39 +0200

    create shopping list

--- src/pages/ShoppingPage.js
+++ src/pages/ShoppingPage.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import ShoppingItem from '../components/ShoppingItem/ShoppingItem';
+import React, { useState } from "react";
+import ShoppingItem from "../components/ShoppingItem/ShoppingItem";
 
 const ShoppingPage = () => {
   let list = [
     {
-      title: 'Bread',
+      title: "Bread",
       done: false,
     },
     {
-      title: 'Butter',
+      title: "Butter",
       done: true,
     },
     {
-      title: 'Eggs',
+      title: "Eggs",
       done: false,
-    }
+    },
   ];
+
+  const [todoList, setTodoList] = useState(list);
+  const [todoInput, setTodoInput] = useState("");
+  const [todoInputDone, setTodoInputDone] = useState(false);
 
   // function renderShoppingList(arr) {
   //   let listElement = arr.map((item, index) => <ShoppingItem key={index} title={item.title} />);
@@ -23,18 +27,66 @@ const ShoppingPage = () => {
   //   return listElement;
   // }
 
+  const addItemHandler = (event) => {
+    event.preventDefault();
+    // const inputValue = event.target.elements.shoppingItem.value;
+    // const inputDone = event.target.elements.shoppingItemDone.checked;
+
+    // const newItem = {
+    //   title: inputValue,
+    //   done: inputDone,
+    // };
+
+    setTodoList((prevState) => [
+      { title: todoInput, done: todoInputDone },
+      ...prevState,
+    ]);
+
+    setTodoInput('');
+    setTodoInputDone(false);
+  };
+
   return (
-    <div className='page-content'>
-      <h2 className='page-title'>{list.length > 0 ? 'Shopping List' : 'No items'}</h2>
-      {list.length > 0 && (
-        <div className='shopping-list'>
+    <div className="page-content">
+      <h2 className="page-title">
+        {todoList.length > 0 ? "Shopping List" : "No items"}
+      </h2>
+
+      <form className="shopping-form" onSubmit={addItemHandler}>
+        <div className="form-control">
+          <label htmlFor="shopping-item">Add an item:</label>
+          <input
+            type="text"
+            name="shoppingItem"
+            id="shopping-item"
+            value={todoInput}
+            onChange={(e) => setTodoInput(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="shopping-item-done">Done:</label>
+          <input
+            type="checkbox"
+            name="shoppingItemDone"
+            id="shopping-item-done"
+            checked={todoInputDone}
+            onChange={(e) => setTodoInputDone(e.target.checked)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+
+      {todoList.length > 0 && (
+        <div className="shopping-list">
           {/* {renderShoppingList(list)} */}
 
-          {list.map((item, index) => <ShoppingItem key={index} title={item.title} done={item.done} />)}
+          {todoList.map((item, index) => (
+            <ShoppingItem key={index} title={item.title} done={item.done} />
+          ))}
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default ShoppingPage
+export default ShoppingPage;
